72a2708b09c3618a7706840519a12fbd3e877a0f
NikitaPilez <pileznikita99@gmail.com>
2022-09-26 12:50:48 +0300

    Fix deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+tmp=/dev/shm/cost-monitoring
+rm -rf ${tmp} && \
+git clone -l -s . ${tmp} -b master && \
+cd ${tmp} && \
+echo "Building..." && \
 composer i --no-dev --optimize-autoloader && \
-rsync -av --delete --exclude ".env" --exclude "/storage" ./ $1@$2:backend/ && \
-ssh $1@$2 "cd backend && php artisan migrate && php artisan optimize && php artisan route:cache && php artisan cache:clear" && \
-echo 'DONE' && \
-composer i
+rm -rf .env.example .gitattributes composer.lock .git* README.md && \
+echo "Deploying..." && \
+rsync -av --delete --exclude ".env" --exclude "/storage" ./ user@143.198.113.70:backend/ && \
+ssh user@143.198.113.70 "cd backend && php artisan migrate && php artisan optimize && php artisan route:cache && php artisan cache:clear" && \
+echo "Done in ${SECONDS} sec."
