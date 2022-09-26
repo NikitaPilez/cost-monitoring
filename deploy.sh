41468b09629234093f557d126dc244d02ad618b9
NikitaPilez <pileznikita99@gmail.com>
2022-09-26 11:34:14 +0300

    Fix deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 composer i --no-dev --optimize-autoloader && \
-rsync -av --delete --exclude ".env" --exclude "/storage" ./ user@143.198.113.70:backend/ && \
+rsync -av --delete --exclude ".env" --exclude "/storage" ./ $1@$2:backend/ && \
 ssh $1@$2 "cd backend && php artisan migrate && php artisan optimize && php artisan route:cache && php artisan cache:clear" && \
 echo 'DONE' && \
 composer i
