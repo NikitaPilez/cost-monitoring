0425701b4032f0e0e2069d53fa67cc43d8ec6e11
NikitaPilez <pileznikita99@gmail.com>
2022-10-07 15:38:33 +0300

    Pm2 config

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ composer i --no-dev --optimize-autoloader && \
 rm -rf .env.example .gitattributes composer.lock .git* README.md && \
 echo "Deploying..." && \
 rsync -av --delete --exclude ".env" --exclude "/storage" ./ user@142.93.102.201:backend/ && \
-ssh user@142.93.102.201 "cd backend && php artisan migrate && php artisan optimize && php artisan route:cache && php artisan cache:clear" && \
+ssh user@142.93.102.201 "cd backend && php artisan migrate && php artisan optimize && php artisan route:cache && php artisan cache:clear && pm2 restart all" && \
 echo "Done in ${SECONDS} sec."
