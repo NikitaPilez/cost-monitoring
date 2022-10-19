08a9f2fe8d552f2145cdefba5abc3a385a38a3d9
NikitaPilez <pileznikita99@gmail.com>
2022-10-19 17:22:34 +0300

    Test

--- tests/Feature/TransformSmsTest.php
+++ tests/Feature/TransformSmsTest.php
@@ -12,53 +12,47 @@ class TransformSmsTest extends TestCase
     /**
      * A basic feature test example.
      *
+     * @dataProvider dataProvider
+     *
      * @return void
      */
-    public function test_example()
+    public function test_example($body, $amount, $place, $balance, $buyAt, $isAccrual)
     {
         $action = new ProcessingAction();
-        foreach ($this->mockData() as $data) {
-            $transformSms = $action->getTransformSms($data['body']);
-            $this->assertEquals($data['result']['amount'], $transformSms['amount']);
-            $this->assertEquals($data['result']['place'], $transformSms['place']);
-            $this->assertEquals($data['result']['balance'], $transformSms['balance']);
-            $this->assertEquals($data['result']['buyAt'], $transformSms['buyAt']);
-            $this->assertEquals($data['result']['isAccrual'], $transformSms['isAccrual']);
-        }
+        $transformSms = $action->getTransformSms($body);
+        $this->assertEquals($amount, $transformSms['amount']);
+        $this->assertEquals($place, $transformSms['place']);
+        $this->assertEquals($balance, $transformSms['balance']);
+        $this->assertEquals($buyAt, $transformSms['buyAt']);
+        $this->assertEquals($isAccrual, $transformSms['isAccrual']);
     }
 
-    public function mockData()
+    public function dataProvider()
     {
         return [
             [
-                'body' => 'Blabla 4.5241' . PHP_EOL . 'Oplata' . PHP_EOL . 'Uspeshno' . PHP_EOL . 'Summa:5.08 BYN' . PHP_EOL . 'Ostatok:604.59 BYN' . PHP_EOL . 'STOLOVAYA BAPB' . PHP_EOL . '20.09.2022 13:03:29',
-                'result' => [
-                    'amount' => '5.08',
-                    'place' => 'STOLOVAYA BAPB',
-                    'balance' => '604.59',
-                    'buyAt' => '20.09.2022 13:03:29',
-                    'isAccrual' => 0
-                ]
+                'Blabla 4.5241' . PHP_EOL . 'Oplata' . PHP_EOL . 'Uspeshno' . PHP_EOL . 'Summa:5.08 BYN' . PHP_EOL . 'Ostatok:604.59 BYN' . PHP_EOL . 'STOLOVAYA BAPB' . PHP_EOL . '20.09.2022 13:03:29',
+                '5.08',
+                'STOLOVAYA BAPB',
+                '604.59',
+                '20.09.2022 13:03:29',
+                0
             ],
             [
-                'body' => 'Karta 4.5241' . PHP_EOL . 'Spisanie' . PHP_EOL . 'Uspeshno' . PHP_EOL . 'Summa:32.09 BYN' . PHP_EOL . 'Ostatok:412.45 BYN' . PHP_EOL . 'POPOLNENIE SCHETA: 91KBYN-C745D8' . PHP_EOL . '18.10.2022 14:52:11',
-                'result' => [
-                    'amount' => '32.09',
-                    'place' => 'POPOLNENIE SCHETA: 91KBYN-C745D8',
-                    'balance' => '412.45',
-                    'buyAt' => '18.10.2022 14:52:11',
-                    'isAccrual' => 0
-                ]
+                'Karta 4.5241' . PHP_EOL . 'Spisanie' . PHP_EOL . 'Uspeshno' . PHP_EOL . 'Summa:32.09 BYN' . PHP_EOL . 'Ostatok:412.45 BYN' . PHP_EOL . 'POPOLNENIE SCHETA: 91KBYN-C745D8' . PHP_EOL . '18.10.2022 14:52:11',
+                '32.09',
+                'POPOLNENIE SCHETA: 91KBYN-C745D8',
+                '412.45',
+                '18.10.2022 14:52:11',
+                0
             ],
             [
-                'body' => 'Karta 4.5241' . PHP_EOL . 'Postuplenie' . PHP_EOL . 'Uspeshno' . PHP_EOL . 'Summa:45.32 BYN' . PHP_EOL . 'Ostatok:412.45 BYN' . PHP_EOL . '18.10.2022 14:52:11',
-                'result' => [
-                    'amount' => '45.32',
-                    'place' => 'Postuplenie',
-                    'balance' => '412.45',
-                    'buyAt' => '18.10.2022 14:52:11',
-                    'isAccrual' => 1
-                ]
+            'Karta 4.5241' . PHP_EOL . 'Postuplenie' . PHP_EOL . 'Uspeshno' . PHP_EOL . 'Summa:45.32 BYN' . PHP_EOL . 'Ostatok:412.45 BYN' . PHP_EOL . '18.10.2022 14:52:11',
+                '45.32',
+                'Postuplenie',
+                '412.45',
+                '18.10.2022 14:52:11',
+                1
             ]
         ];
     }
